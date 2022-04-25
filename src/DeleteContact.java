57d3a90ae4f29a9ac887e93f137670c7a9dc6035
Chris Eliason <98778355+nosaile@users.noreply.github.com>
2022-04-25 17:02:52 -0500

    alot of stuff

--- src/DeleteContact.java
+++ src/DeleteContact.java
@@ -2,23 +2,29 @@ import jdk.swing.interop.SwingInterOpUtils;
 
 import java.io.*;
 import java.nio.file.Files;
+import java.util.Locale;
 
 
 public class DeleteContact {
 
     public static void deleteFeature() throws IOException {
         System.out.println("--- DELETE CONTACT ---");
+        RenderContacts.render();
         System.out.println("First Name?");
-        String userDeleteContactFirstName = AddContact.scanner.nextLine();
-        System.out.println("Last Name?");
-        String userDeleteContactLastName = AddContact.scanner.nextLine();
+        String userDeleteContactFirstName = AddContact.scanner.next();
+//        System.out.println("Last Name?");
+//        String userDeleteContactLastName = AddContact.scanner.nextLine();
+//        System.out.println("Number?");
+//        int userDeleteContactNumber = AddContact.scanner.nextInt();
+//        System.out.println("Who do you want to delete? (choose which number)");
+//        int userDeleteIdNumber = AddContact.scanner.nextInt();
 
 
         File file = new File("contacts.txt");
         File temp = new File("contacts-temp.txt");
         PrintWriter deleteContact = new PrintWriter(new FileWriter(temp));
           Files.lines(file.toPath())
-               .filter(line -> !line.contains(userDeleteContactFirstName + " " + userDeleteContactLastName))
+               .filter(line -> !line.contains(userDeleteContactFirstName.toUpperCase(Locale.ROOT)))
                 .forEach(deleteContact::println);
         deleteContact.flush();
         deleteContact.close();
--- src/MainMenu.java
+++ src/MainMenu.java
@@ -1,35 +1,36 @@
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.InputMismatchException;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class MainMenu {
     public MainMenu() {
 
     }
-
+   protected static int number;
     public static void displayMenu() throws IOException {
         Scanner scanner = new Scanner(System.in);
         System.out.println("1: view contacts\n2: add new\n3: search contacts\n4: delete contacts\n5: exit app\nEnter an option (1, 2, 3, 4 ,5)");
-        int menuOption = scanner.nextInt();
-        if (menuOption != 1 && menuOption != 2 && menuOption != 3 && menuOption != 4 && menuOption != 5) {
+        String menuOption = scanner.nextLine();
+        if (!Objects.equals(menuOption, "1") && !Objects.equals(menuOption, "2") && !Objects.equals(menuOption, "3") && !Objects.equals(menuOption, "4") && !Objects.equals(menuOption, "5")) {
             System.out.println("--- Invalid Response ---");
             MainMenu.displayMenu();
-        } else if (menuOption == 1) {
+        } else if (menuOption.equals("1")) {
             RenderContacts.render();
             System.out.println("\n");
             MainMenu.displayMenu();
-        } else if (menuOption == 2) {
+        } else if (menuOption.equals("2")) {
             AddContact.add();
             System.out.println("--- contact list updated ---\n");
             MainMenu.displayMenu();
-        } else if (menuOption == 3) {
+        } else if (menuOption.equals("3")) {
             SearchContacts.searchFeature();
             MainMenu.displayMenu();
-        }else if(menuOption == 4){
+        }else if(menuOption.equals("4")){
             DeleteContact.deleteFeature();
             MainMenu.displayMenu();
-        }else if (menuOption == 5){
+        }else if (menuOption.equals("5")){
             System.out.println("Goodbye :)");
         }else{
             System.out.println("--- INVALID RESPONSE ---");
